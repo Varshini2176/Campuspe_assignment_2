2ea98fd20a1c4156c4bc15690cf11f688fd31e11
Varshini2176 <varshini2176@gmail.com>
2026-02-24 12:56:21 +0530

    Update and rename q10_simpleatm_simulator.py to Q10_simpleatm_simulator.py

--- Q10_simpleatm_simulator.py
+++ Q10_simpleatm_simulator.py
@@ -1,7 +1,7 @@
 balance = 10000
 
 while True:
-    print("\nATM SIMULATOR")
+    print("\nsimple atm simulator")
     print("1. Check Balance")
     print("2. Deposit Money")
     print("3. Withdraw Money")
@@ -11,30 +11,30 @@ while True:
         choice = int(input("Enter your choice (1-4): "))
 
         if choice == 1:
-            print(f"Current Balance: ₹{balance}")
+            print(f"Current Balance: {balance}")
 
         elif choice == 2:
-            amount = float(input("Enter amount to deposit: ₹"))
+            amount = float(input("Enter amount to deposit: "))
             if amount > 0:
                 balance += amount
                 print("Deposit successful!")
-                print(f"Updated Balance: ₹{balance}")
+                print(f"Updated Balance: {balance}")
             else:
                 print("Deposit amount must be positive.")
 
         elif choice == 3:
-            amount = float(input("Enter amount to withdraw: ₹"))
+            amount = float(input("Enter amount to withdraw: "))
 
             if amount <= 0:
                 print("Withdrawal amount must be positive.")
             elif amount > balance:
                 print("Insufficient balance!")
             elif balance - amount < 500:
-                print("Minimum balance of ₹500 must be maintained.")
+                print("Minimum balance of 500 must be maintained.")
             else:
                 balance -= amount
                 print("Withdrawal successful!")
-                print(f"Updated Balance: ₹{balance}")
+                print(f"Updated Balance: {balance}")
 
         elif choice == 4:
             print("Thank you for using ATM. Goodbye!")
@@ -45,3 +45,4 @@ while True:
 
     except ValueError:
         print("Invalid input! Please enter numeric values.")
+
